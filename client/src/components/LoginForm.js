dcb7a5ebc94bdff4ee9b39e251a17d0644b850fe
Alex Glaubitz <e.pirazzi@gmail.com>
2022-07-22 02:00:47 -0400

    updated loginUser import, added setContext import to App.js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/queries'
+import { loginUser } from '../utils/mutations'
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
